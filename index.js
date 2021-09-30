e89fb6f06d16dbf52ed4de134cdaaa88f1c61e02
Brandon May <battleb2014@gmail.com>
2021-09-30 12:56:00 -0400

    C:/Program Files/Git/

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-server.get('/hello', (req, res) => {
-    res.send('<h1>Hello there!</h1>')
+server.get('/', (req, res) => {
+    res.json({
+        message: 'Web46 Rules!!!'
+    })
 })
+
+server.get('/hello', (req, res) => {
+    res.send('<h1>Hello there!</h1>');
+});
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`listening on ${port}`);
-})
+});
